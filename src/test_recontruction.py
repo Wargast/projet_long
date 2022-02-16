7bc76cf240d3b0d8addaa20cd5b0343054dd91e5
Luc Baudinaud <luc.baudinaud@etu.enseeiht.fr>
2022-02-16 10:04:57 +0100

    fix vector dim bug and add plotting

--- src/test_recontruction.py
+++ src/test_recontruction.py
@@ -6,8 +6,8 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 import cv2
-
-######## calib.txt #########
+import pyvista as pv
+######## calib.txt ladder1 #########
 
 fx = 1733.68
 fy = 1733.68
@@ -21,6 +21,7 @@ height = 1080
 ndisp = 100
 vmin = 20
 vmax = 70
+
 ############################
 
 Q = np.array(
@@ -47,11 +48,9 @@ for u in range(pfm0.shape[0]):
         D = pfm0[u,v]
         p = np.dot(Q, np.array([[u,v,D,1]]).transpose())
         q = p/p[3]
-        # print('p', p)
-        # print('q', q)
-        # exit() 
-        points.append(q.transpose())
+
+        points.append(q[:3].transpose())
         print(f"{round((u*pfm0.shape[1]+v)/tot_point*100)}%", end="\r")
 
-mesh_points = np.concatenate(points)
-
+point_cloud = pv.PolyData( np.concatenate(points, axis=0) )
+point_cloud.plot(eye_dome_lighting=True)
