29cc417c5113be98f580e125dc6960a88c47fa5e
Luc Baudinaud <luc.baudinaud@etu.enseeiht.fr>
2022-02-16 12:43:01 +0100

    refactor 3D recontruction with numpy operations

--- src/test_recontruction.py
+++ src/test_recontruction.py
@@ -43,14 +43,34 @@ pfm1 = loader.load_pfm(dataset_path/'disp1.pfm')
 
 points = []
 tot_point = pfm0.shape[0] * pfm0.shape[1]
-for u in range(pfm0.shape[0]):
-    for v in range(pfm0.shape[1]):
-        D = pfm0[u,v]
-        p = np.dot(Q, np.array([[u,v,D,1]]).transpose())
-        q = p/p[3]
 
-        points.append(q[:3].transpose())
-        print(f"{round((u*pfm0.shape[1]+v)/tot_point*100)}%", end="\r")
 
-point_cloud = pv.PolyData( np.concatenate(points, axis=0) )
+xv, yv = np.meshgrid(range(pfm0.shape[0]),range(pfm0.shape[1]), indexing='ij')
+M = np.concatenate(
+    [
+        np.expand_dims(xv, axis=2),
+        np.expand_dims(yv, axis=2),
+        np.expand_dims(pfm0, axis=2),
+        np.expand_dims(np.ones(pfm0.shape), axis=2),
+    ], axis= 2)
+
+points = np.matmul(M,Q.transpose()).reshape(-1,4)
+points_p = np.multiply(points,(1/points[:, 3])[:,np.newaxis])
+
+
+# for u in range(pfm0.shape[0]):
+#     for v in range(pfm0.shape[1]):
+#         D = pfm0[u,v]
+#         p = np.dot(Q, np.array([[u,v,D,1]]).transpose())
+#         q = p/p[3]
+
+#         points.append(q[:3].transpose())
+#         print(f"{round((u*pfm0.shape[1]+v)/tot_point*100)}%", end="\r")
+# point_p = np.concatenate(points, axis=0)
+
+# point_cloud = pv.PolyData(  )
+
+
+
+point_cloud = pv.PolyData(points_p[:, :3])
 point_cloud.plot(eye_dome_lighting=True)
