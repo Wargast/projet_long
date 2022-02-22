34a68fdaefeb4b96a63e97ae8312652c04f075f1
Luc Baudinaud <luc.baudinaud@etu.enseeiht.fr>
2022-02-22 16:59:31 +0100

    add wrapper to create stereo from param file

--- src/test_recontruction.py
+++ src/test_recontruction.py
@@ -9,12 +9,14 @@ import pandas as pd
 import numpy as np
 import cv2
 import pyvista as pv
-from setup_tools import parse_calib_file, parse_pickle
+from setup_tools import (parse_calib_file, parse_pickle,
+                        stereoBM_from_file, stereoSGBM_from_file)
+
 ######## var #########
 
 loader = PFMLoader(color=False, compress=False)
 dataset_path = Path("datas/middlebury/chess1")
-tune_params = Path("results/param.pkl")
+tuned_params = Path("results/param.pkl")
 calib_params = parse_calib_file(dataset_path/'calib.txt')
 pprint(calib_params)
 Q = np.array(
@@ -25,7 +27,6 @@ Q = np.array(
         [0,         0,  1/calib_params["baseline"],                     0 ],
     ]
 )
-
 
 
 ############################
@@ -53,27 +54,12 @@ disparity = loader.load_pfm(dataset_path/'disp0.pfm')
 img0 = cv2.imread((dataset_path/"im0.png").as_posix(), cv2.IMREAD_GRAYSCALE)
 img1 = cv2.imread((dataset_path/"im1.png").as_posix(), cv2.IMREAD_GRAYSCALE)
 
-stereo_params = parse_pickle(tune_params)
-pprint(stereo_params)
 
-
-
-stereo = cv2.StereoBM_create()
-stereo = cv2.StereoSGBM_create()
-stereo.setNumDisparities(stereo_params["numDisparities"])
-stereo.setBlockSize(stereo_params["blockSize"])
-# stereo.setPreFilterType(param["preFilterType"])
-# stereo.setPreFilterSize(param["preFilterSize"])
-# stereo.setPreFilterCap(param["preFilterCap"])
-# stereo.setTextureThreshold(param["textureThreshold"])
-stereo.setUniquenessRatio(stereo_params["uniquenessRatio"])
-stereo.setSpeckleRange(stereo_params["speckleRange"])
-stereo.setSpeckleWindowSize(stereo_params["speckleWindowSize"])
-stereo.setDisp12MaxDiff(stereo_params["disp12MaxDiff"])
-stereo.setMinDisparity(stereo_params["minDisparity"])
+stereo = stereoBM_from_file(tuned_params)
+stereo = stereoSGBM_from_file(tuned_params)
 
 # Compute the disparity image
-disparity = stereo.compute(img0, img1)/16*2.
+disparity = stereo.compute(img0, img1)/16.
 print(f"bound: [{disparity.min()}, {disparity.max()}]")
 disparity[disparity <= 80] = np.nan
 print(f"bound: [{disparity.min()}, {disparity.max()}]")
