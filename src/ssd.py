b6fe20fe74d870fd15d7e05c93bdb4129f74eb20
apigneux <alice.pigneux@gmail.com>
2022-02-22 17:28:08 +0100

    Merge branch 'main' of github.com:Wargast/projet_long into main

--- src/ssd.py
+++ src/ssd.py
@@ -1,87 +1,99 @@
+from matplotlib.pyplot import axis
 import numpy as np
+from tqdm import tqdm
 import cv2
-import math
 import time
 
-im_2 = cv2.imread('../datas/middlebury/artroom1/im0.png', cv2.IMREAD_GRAYSCALE)
-im_ref = cv2.imread('../datas/middlebury/artroom1/im0.png', cv2.IMREAD_GRAYSCALE)
+
+def faster_census_transform(i: np.ndarray, block_size: int = 3) -> np.ndarray:
+    if block_size % 2 == 0:
+        raise ValueError("Block size must be an odd number")
+
+    (h, l) = i.shape
+
+    # only consider the pixels where the block doesn't go out of the image
+    h_cropped = h - block_size + 1
+    l_cropped = l - block_size + 1
+
+    # get all blocks of the image of size (block_size x block_size)
+    blocks = np.lib.stride_tricks.sliding_window_view(i, (block_size, block_size))
+
+    blocks = blocks.reshape(h_cropped, l_cropped, block_size ** 2)
+    return blocks
+
+def faster_census_matching(
+    i_left: np.ndarray,
+    i_right: np.ndarray,
+    block_size: int = 15,
+    max_disparity: int = 64,
+    cost_threshold: int = 125,
+) -> np.ndarray:
+
+    (h1, l1) = i_right.shape
+    (h2, l2) = i_left.shape
+
+    if h1 != h2:
+        raise ValueError("Both images must have the same height")
+
+    # only consider the pixels where the block doesn't go out of the image
+    h1_cropped = h1 - block_size + 1
+    l1_cropped = l1 - block_size + 1
+
+    # calculate the bistrings for the first image :
+    bit_strings_1 = faster_census_transform(i_right, block_size=block_size)
+
+    l2_cropped = l2 - block_size + 1
+
+    # calculate the bitstrings for the second image
+    bit_strings_2 = faster_census_transform(i_left, block_size=block_size)
+
+    # disparity_map = np.full((h1, l1), np.inf)
+
+    error_map = np.full(
+        (h1_cropped, l1_cropped, max_disparity + 1),
+        np.inf,
+    )
+
+    for d in tqdm(range(max_disparity + 1)):
+        right = bit_strings_1[:, 0 : min(l2_cropped - d, l1_cropped), :]
+        left = bit_strings_2[:, d:, :]
+
+        costs = np.abs(right-left).sum(axis=2)
+        error_map[:, 0 : min(l2_cropped - d, l1_cropped), d] = costs
+
+    return error_map
+
+def disparity_from_error_map(
+    error_map: np.ndarray, block_size: int, cost_threshold: int = 125
+) -> np.ndarray:
+    (h_cropped, l_cropped, _) = error_map.shape
+    half_block_size = block_size // 2
+
+    errors = np.min(error_map, axis=2)
+    disparity_map = np.argmin(error_map, axis=2).astype(np.float32)
+
+    disparity_map[errors > cost_threshold] = np.inf
+
+    disparity_map = np.pad(
+        disparity_map,
+        (half_block_size, half_block_size),
+        mode='constant',
+        constant_values=np.inf
+    )
+
+    return disparity_map
 
 
+if __name__ == "__main__":
+    i_left = cv2.imread("datas/middlebury/artroom1/im0.png", cv2.IMREAD_GRAYSCALE)
+    i_right = cv2.imread("./datas/middlebury/artroom1/im1.png", cv2.IMREAD_GRAYSCALE)
 
-def ssd_disparites(im_ref, im_2,taille_f = 15):
-    """AI is creating summary for ssd_disparites
+    error_map = faster_census_matching(
+        i_left, i_right, block_size=15, max_disparity=128
+    )
 
-    Args:
-        im_ref ([type]): [description]
-        im_2 ([type]): [description]
-        taille_f (int, optional): taille de la fenetre. Defaults to 15.
-    """
+    disparity_map = disparity_from_error_map(
+        error_map, block_size=15, cost_threshold=1000000000
+    )
 
-    # Paramètres
-
-    l = im_ref.shape[0]
-    c = im_ref.shape[1]
-    f = math.floor(taille_f/2)
-
-
-
-    ## k = 0
-    blocks_ref = np.lib.stride_tricks.sliding_window_view(im_ref,(taille_f,taille_f))
-    blocks = np.lib.stride_tricks.sliding_window_view(im_2,(taille_f,taille_f))
-
-    blocks_0 = np.ones(blocks.shape)*np.inf    
-    error = np.square(blocks_ref-blocks).sum(axis=2).sum(axis=2)
-    disparity = np.zeros((blocks.shape[0],blocks.shape[1]))
-    for k in range(1,128):
-        blocks_k = blocks_0
-        # decalage de k
-        print(k)
-        blocks_k[:,:blocks_k.shape[1]-k,:,:] = blocks[:,k:,:,:]
-
-        # Calcul de l'erreur  ssd
-        error_k = np.square(blocks_ref-blocks_k).sum(axis=2).sum(axis=2)
-
-        # Calcul du decalage minimum 
-        disparity[error > error_k] = k
-        error = np.minimum(error,error_k)
-
-    disparity_res = np.ones((l,c))*np.inf
-    disparity_res[f:l-f,f:c-f] = disparity
-    
-    # Création de la carte de disparités
-    # for i in range(l):
-    #     for j in range(c):
-    #         if i > f and i < (l-f) and j > f and j < (c-f):
-    #             bloc0 = im_ref[i-f:i+f,j-f:j+f]
-    #             erreurs = np.zeros((1,128))
-
-
-    # for i in range(l):
-    #     for j in range(c):
-            
-    #         if i > f and i < (l-f) and j > f and j < (c-f):
-    #             # Par bloc avec taille_fenetre = 15
-    #             bloc0 = im_ref[i-f:i+f,j-f:j+f]
-    #             erreurs = np.zeros((1,128))
-                
-    #             for k in range(j,j+128):
-    #                 if k>7 and k<(c-7):
-    #                     val = np.sum(np.square(im_2[i-f:i+f,k-f:k+f]-bloc0))
-    #                     np.append(erreurs, val)
-                        
-    #                 else:
-    #                     np.append(erreurs,np.inf)       
-    #             index_erreur = np.argmin(erreurs)
-    #             disparity[i,j] = index_erreur - j
-            
-    #         else: 
-    #             disparity[i,j] = np.inf
-                
-    np.save("disparity_ssd.npy", disparity_res)
-    return disparity_res
-
-
-debut = time.time()
-ssd_disparites(im_ref=im_ref, im_2=im_2)
-fin = time.time() - debut
-print(fin)
+    np.save("disp_sad_artroom1.npy", disparity_map)
